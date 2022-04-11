5468dd93ce23fed38d3c2533fca35521ce9dad71
giminose <fearlo1206@gmail.com>
2022-04-11 23:40:23 +0800

    delay 2 clock

--- mac/mac.v
+++ mac/mac.v
@@ -7,49 +7,55 @@ input  stall;
 input  [2:0] instruction;
 output reg [31:0] result;
 output reg [7:0] protect;
-reg signed [31:0] reg_result_tmp;
-reg signed [31:0] reg_result;
+reg signed [31:0] reg_result_tmp_1;
+reg signed [31:0] reg_result_tmp_2;
+reg signed [31:0] reg_result_tmp_3;
 reg [7:0] reg_protect;
 
   always@(posedge clk or negedge reset_n) begin
     if(~reset_n) begin
-      reg_result <= reg_result_tmp;
-      result <= reg_result;
-      protect <= reg_protect;
+      reg_result_tmp_1 <= 32'b0;
+      reg_result_tmp_2 <= 32'b0;
+      reg_result_tmp_3 <= 32'b0;
+      result <= 32'b0;
+      protect <= 8'b0;
     end else begin
-      protect = 8'b0;
-      result = 32'b0;
+      reg_result_tmp_2 <= reg_result_tmp_1;
+      reg_result_tmp_3 <= reg_result_tmp_2;
+      result <= reg_result_tmp_3;
     end
   end
 
   always@(*) begin
-    reg_protect = protect;
+    // reg_protect = protect;
     case(instruction)
       3'b000: begin
-        reg_result_tmp = 32'b0;
-        reg_protect = 8'b0;
+        // reg_result_tmp = 32'b0;
+        // reg_protect = 8'b0;
+        reg_result_tmp_1 = multiplier * multiplicand;
       end
       3'b001: begin
-         reg_result_tmp = multiplier * multiplicand;
+         reg_result_tmp_1 = multiplier * multiplicand;
       end
       3'b010: begin
-         reg_result_tmp = multiplier * multiplicand;
+         reg_result_tmp_1 = multiplier * multiplicand;
       end
       3'b011: begin
-         reg_result_tmp = multiplier * multiplicand;
+         reg_result_tmp_1 = multiplier * multiplicand;
       end
       3'b100: begin
-        reg_result_tmp = 32'b0;
-        reg_protect = 8'b0;
+        // reg_result_tmp = 32'b0;
+        // reg_protect = 8'b0;
+        reg_result_tmp_1 = multiplier * multiplicand;
       end
       3'b101: begin
-         reg_result_tmp = multiplier * multiplicand;
+         reg_result_tmp_1 = multiplier * multiplicand;
       end
       3'b110: begin
-         reg_result_tmp = multiplier * multiplicand;
+         reg_result_tmp_1 = multiplier * multiplicand;
       end
       3'b111: begin
-         reg_result_tmp = multiplier * multiplicand;
+         reg_result_tmp_1 = multiplier * multiplicand;
       end
     endcase
   end
